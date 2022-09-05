65561ebc2cf8e852420b93eb302da372e18050da
EmTeddyTee <michaeltaylor91@live.com>
2022-09-05 15:37:02 +0100

    05/09/22 15:23

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("")
+# We can use formatted strings to clean up code
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder'
+print(message)
+
+# We create two placeholders within our string and insert variable names to fill these gaps using {}
+msg = f'{first} [{last}] is a coder'
+print(msg)
+# the result is a cleaner and more legible form of presenting the same code and output
