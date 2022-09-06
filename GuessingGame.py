35918539f6cb996651aeb067171e10a792435d48
EmTeddyTee <michaeltaylor91@live.com>
2022-09-06 13:58:01 +0100

    06/09/22 13:26

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,21 +1,21 @@
 import random
-ran = 10
-#ran = random.randint(1, 25)
-cnt = 5
-while cnt >= 1:
-    ans = int(input(f'You have {cnt} guesses! Enter your guess: '))
-    if ans == (ran <= ran - 4) or ans == ran + 1 or ans == ran - 2 or ans == ran + 2:
+
+ran = random.randint(1, 25)
+guess_limit = 5
+while guess_limit >= 1:
+    ans = int(input(f'You have {guess_limit} guesses! Enter your guess: '))
+    if ans == (ran_num <= ran_num - 4) or ans == ran_num + 1 or ans == ran_num - 2 or ans == ran_num + 2:
         print("Hot")
-        cnt -= 1
-    elif ans == ran + 3 or ans == ran - 3:
+        guess_limit -= 1
+    elif ans == ran_num + 3 or ans == ran_num - 3:
         print("Warm")
-        cnt -= 1
-    elif ans == ran + 4 or ans == ran - 4 or ans == ran + 5 or ans == ran - 5:
+        guess_limit -= 1
+    elif ans == ran_num + 4 or ans == ran_num - 4 or ans == ran_num + 5 or ans == ran_num - 5:
         print("Cold")
-        cnt -= 1
-    elif ans == ran:
-        print(f'You win! The answer was indeed {ran}')
+        guess_limit -= 1
+    elif ans == ran_num:
+        print(f'You win! The answer was indeed {ran_num}')
         break
     else:
         print("Freezing")
-        cnt -= 1
+        guess_limit -= 1
