b5657b7a6b5a060dac9575af182ec985c1df2430
EmTeddyTee <michaeltaylor91@live.com>
2022-09-06 14:42:08 +0100

    06/09/22 14:42

---
+++ ArraysLists.py
@@ -0,0 +1,13 @@
+my_intlist = [2, 4, 6, 8, 10]
+my_strlist = ["Hi,", "how", "are", "you"]
+
+for n in range(1, 6):
+    print(n)
+print("")
+
+total = 0
+prices = [23.99, 12, 7.99]
+for price in prices:
+    total += price
+    print(f'£{total:.2f}')  #string format to show only 2 places
+print(f'Your final total is: £{total}')
