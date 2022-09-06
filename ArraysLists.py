62618a4875e97bf612b542b9e57ce53a1d511452
EmTeddyTee <michaeltaylor91@live.com>
2022-09-06 14:46:55 +0100

    06/09/22 14:46

--- ArraysLists.py
+++ ArraysLists.py
@@ -3,7 +3,7 @@ my_strlist = ["Hi,", "how", "are", "you"]
 
 for n in range(1, 6):
     print(n)
-print("")
+print("-------------------------")
 
 total = 0
 prices = [23.99, 12, 7.99]
@@ -11,3 +11,4 @@ for price in prices:
     total += price
     print(f'£{total:.2f}')  #string format to show only 2 places
 print(f'Your final total is: £{total}')
+print("-----------------------")
---
+++ NestedLoop.py
@@ -0,0 +1,5 @@
+for x in range(4):
+    print("Outer loop control")
+    for y in range(3):
+        print("Inner loop control")
+        print(f'X = {x}, Y = {y}')
