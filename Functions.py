906650a76126e6f7b490169f2f481c3f1f56d3f6
EmTeddyTee <michaeltaylor91@live.com>
2022-09-08 13:25:48 +0100

    08/09/22 13:25

--- Functions.py
+++ Functions.py
@@ -6,3 +6,27 @@ def greet_user(first_name, last_name):
 print("Start")
 greet_user("John", "Smith")
 print("Finish")
+
+
+def square(num):
+    return num * num
+
+
+print(square(3))
+
+
+message = input(">")
+def emoji_converter(message):
+    words = message.split(' ')
+    print(words)
+    emojis = {
+        ":)": "😊",
+        ":(": "😔",
+        ":|": "😐"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+print(emoji_converter(message))
