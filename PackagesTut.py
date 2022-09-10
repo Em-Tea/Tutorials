c74c23b92fd1b8ca0fcd326c7848f3ab2bd7e9c9
EmTeddyTee <michaeltaylor91@live.com>
2022-09-10 11:55:01 +0100

    10/09/22 11:54

---
+++ PackagesTut.py
@@ -0,0 +1,4 @@
+# import ecommerce.shipping
+from ecommerce import shipping # We use this to import all methods within shipping to reduce code
+
+shipping.calc_shipping()
---
+++ ecommerce/__init__.py
@@ -0,0 +1,6 @@
+"""
+When using packages we first create another directory, separate from our main directory.
+We create a file named __init__.py within this directory, which informs py to consider this a package.
+Within Pycharm there exists a shortcut to create packages which comes with an __init__ file.
+
+"""
